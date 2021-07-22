68aa7a64914c46a0a04fe2cd47fac1a2c011aba8
Fabricio Jacobina <fjacobinam@gmail.com>
2021-07-22 17:19:21 -0300

    O que é oDOM

---
+++ JavaScript-ES6/002-DOM-Iniciantes/o-que-e-o-DOM/script.js
@@ -0,0 +1,11 @@
+//window.alert('Estudando DOM');
+
+const href = window.location.href;
+
+console.log(href);
+if (
+  href === "http://127.0.0.1:5500/002-DOM-Iniciantes/o-que-e-o-DOM/index.html"
+) {
+  console.log("O endereço é igual!");
+}
+const titulo = document.querySelector(".titulo");
