8205b586c43f6ae1adf2181d26e2e1386c36c871
Fabricio Jacobina <fjacobinam@gmail.com>
2021-07-25 11:52:04 -0300

    DOM

--- JavaScript-ES6/002-DOM-Iniciantes/o-que-e-o-DOM/script.js
+++ JavaScript-ES6/002-DOM-Iniciantes/o-que-e-o-DOM/script.js
@@ -9,3 +9,17 @@ if (
   console.log("O endereço é igual!");
 }
 const titulo = document.querySelector(".titulo");
+
+// Retorne a url da página atual utilizando o objeto window
+console.log(`A url da página é ${window.location.href}`);
+
+// Seleciona o primeiro elemento da página que possua a classe ativo
+console.log(window.document.querySelector(".ativo"));
+
+// Retorna a linguagem do navegador
+console.log(
+  `O navegador está utilizando a linguagem ${window.navigator.language}`
+);
+
+// Retorna a largura da janela
+console.log(` A largura atual da janela é de ${window.innerWidth}px `);
