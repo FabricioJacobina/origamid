0d7ba4151a0d004b8eb4545693946193b8a2c436
Fabricio Jacobina <fjacobinam@gmail.com>
2021-11-27 14:00:43 -0300

    27/11/2021

--- JavaScript-ES6/004-Efeitos-DOM/007-Clickoutside/js/script.js
+++ JavaScript-ES6/004-Efeitos-DOM/007-Clickoutside/js/script.js
@@ -3,9 +3,13 @@ import initScrollAnimation from "./modules/animacao-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 initScrollSuave();
 initScrollAnimation();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
+initDropdownMenu();
