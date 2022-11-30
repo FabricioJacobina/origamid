fbd486a3f54fdc070ea6429943fa24e6995f63b6
Fabricio Jacobina <fjacobinam@gmail.com>
2022-11-30 17:17:28 -0300

    useEffect1

--- React/aula013/src/App.js
+++ React/aula013/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Produto from './produto';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [ativo, setAtivo] = React.useState(false);
+
+  return (
+    <div>
+      {ativo && <Produto />}
+      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
+    </div>
+  );
+};
 
 export default App;
