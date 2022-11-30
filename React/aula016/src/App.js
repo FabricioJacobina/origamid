b4de99ec1d4af3e2c61deed36716bfd6cace7879
Fabricio Jacobina <fjacobinam@gmail.com>
2022-11-30 20:02:57 -0300

    useMemo e useCallback

--- React/aula016/src/App.js
+++ React/aula016/src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+function operacaoLenta() {
+  let c;
+  for (let i = 0; i < 100000000; i++) {
+    c = i + i / 10;
+  }
+  return c;
+}
+
 const App = () => {
-  return null;
+  const [contar, setContar] = React.useState(0);
+  const t1 = performance.now();
+  const valor = React.useMemo(() => operacaoLenta(), []);
+  // é mais rápido que
+  // const valor = operacaoLenta();
+  console.log(performance.now() - t1);
+
+  const handleClick = React.useCallback(() => {
+    setContar((contar) => contar + 1);
+  }, []);
+
+  return <button onClick={handleClick}>{contar}</button>;
 };
 
 export default App;
